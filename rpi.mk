cc0fc0b9f103c7e67f1d2a9fb06d129c2de52dba
Chris Jones <cjones.bugs@gmail.com>
2014-08-29 22:06:24 -0700

    Add files and config tweaks needed to launch b2g

--- rpi.mk
+++ rpi.mk
@@ -1,5 +1,13 @@
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+BOARDDIR := device/rpi/rpi
+
+PRODUCT_COPY_FILES := \
+	$(BOARDDIR)/init.rc:root/init.rc \
+	$(BOARDDIR)/vold.fstab:system/etc/vold.fstab
+
+PRODUCT_PROPERTY_OVERRIDES :=
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 PRODUCT_NAME := rpi
 PRODUCT_DEVICE := rpi
