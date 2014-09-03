cf2d462d14f99f71ed42051176cf2a8317c83db7
Chris Jones <cjones.bugs@gmail.com>
2014-09-03 12:27:49 -0700

    Add more bcm2835 config options

--- rpi.mk
+++ rpi.mk
@@ -2,10 +2,20 @@
 BOARDDIR := device/rpi/rpi
 
 PRODUCT_COPY_FILES := \
+	development/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
+	development/data/etc/vold.conf:system/etc/vold.conf \
+	development/tools/emulator/system/camera/media_profiles.xml:system/etc/media_profiles.xml \
+	brcm_usrlib/dag/vmcsx/egl.cfg:system/lib/egl/egl.cfg \
 	$(BOARDDIR)/init.rc:root/init.rc \
 	$(BOARDDIR)/vold.fstab:system/etc/vold.fstab
 
-PRODUCT_PROPERTY_OVERRIDES :=
+PRODUCT_PROPERTY_OVERRIDES := \
+	ro.ril.hsxpa=1 \
+	ro.ril.gprsclass=10
+
+PRODUCT_PACKAGES := \
+	audio.primary.goldfish \
+	libGLES_hgl
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
