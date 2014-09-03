5deb1863d31b34839d616176cb88b3b60eff215a
Chris Jones <cjones.bugs@gmail.com>
2014-09-03 15:39:39 -0700

    These files aren't around.

--- rpi.mk
+++ rpi.mk
@@ -2,9 +2,6 @@
 BOARDDIR := device/rpi/rpi
 
 PRODUCT_COPY_FILES := \
-	development/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
-	development/data/etc/vold.conf:system/etc/vold.conf \
-	development/tools/emulator/system/camera/media_profiles.xml:system/etc/media_profiles.xml \
 	brcm_usrlib/dag/vmcsx/egl.cfg:system/lib/egl/egl.cfg \
 	$(BOARDDIR)/init.rc:root/init.rc \
 	$(BOARDDIR)/vold.fstab:system/etc/vold.fstab
