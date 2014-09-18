a4de6ed5d9aa8d48f2cb52bc066d400844cbf0f0
Chris Jones <cjones.bugs@gmail.com>
2014-09-18 13:02:26 -0700

    Fill the background behind the boot animation to #00539f, to match the frame's background.

--- rpi.mk
+++ rpi.mk
@@ -1,20 +1,6 @@
 
 BOARDDIR := device/rpi/rpi
 
-PRODUCT_COPY_FILES := \
-	brcm_usrlib/dag/vmcsx/egl.cfg:system/lib/egl/egl.cfg \
-	$(BOARDDIR)/init.rc:root/init.rc \
-	$(BOARDDIR)/vold.fstab:system/etc/vold.fstab \
-	$(BOARDDIR)/bootanimation.zip:system/media/bootanimation.zip
-
-PRODUCT_PROPERTY_OVERRIDES := \
-	ro.ril.hsxpa=1 \
-	ro.ril.gprsclass=10
-
-PRODUCT_PACKAGES := \
-	audio.primary.goldfish \
-	libGLES_hgl
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 PRODUCT_NAME := rpi
@@ -24,6 +10,15 @@ PRODUCT_DEVICE := rpi
 # PRODUCT_MANUFACTURER := rpi
 # PRODUCT_RELEASE_NAME := rpi
 
-# TODO: PRODUCT_PROPERTY_OVERRIDES := ...
+PRODUCT_COPY_FILES := \
+	brcm_usrlib/dag/vmcsx/egl.cfg:system/lib/egl/egl.cfg \
+	$(BOARDDIR)/init.rc:root/init.rc \
+	$(BOARDDIR)/vold.fstab:system/etc/vold.fstab \
+	$(BOARDDIR)/bootanimation.zip:system/media/bootanimation.zip
 
-# TODO: PRODUCT_PACKAGES := ...
+PRODUCT_PACKAGES := \
+	audio.primary.goldfish \
+	libGLES_hgl
+
+PRODUCT_PROPERTY_OVERRIDES := \
+	ro.moz.bootanim.bgcolor=0x00539f
