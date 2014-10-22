5ebbac139caed9add934776d722e0c106379654a
Chris Jones <cjones.bugs@gmail.com>
2014-10-22 12:55:43 -0700

    Ensure that eth0 has DNS and gecko assumes it may be online, and remove extraneous file from bootanimation.

--- custom-prefs.js
+++ custom-prefs.js
@@ -1,4 +1,13 @@
 
+// rpi devices have an eth0 interface through an rndis.  But
+// gecko/gonk don't know to configure that yet.  So instead, we
+// attempt to configure it "manually" and tell gecko not to manage
+// offline status, since if we successfully bring up eth0, gecko won't
+// know.
+user_pref("network.gonk.manage-offline-status", false);
+
+// Force-disable accelerated graphics while the GL stack isn't fully
+// functional.
 user_pref("layers.acceleration.force-enabled", false);
 user_pref("layers.acceleration.disabled", true);
 user_pref("layers.offmainthreadcomposition.force-basic", true);
@@ -9,5 +18,4 @@ user_pref("layers.gralloc.disable", true);
 //user_pref("layers.frame-counter", true);
 //user_pref("layers.bench.enabled", true);
 //user_pref("gfx.layerscope.enabled", true);
-//user_pref("network.gonk.manage-offline-status", false);
 user_pref("app.reportCrashes", false);
